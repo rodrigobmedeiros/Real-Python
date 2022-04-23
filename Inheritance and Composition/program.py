b7205de26599cc1f01261c34fa7ebff78426a40c
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-04-23 13:25:29 -0300

    implement a small program just to verify mixin functionality

--- Inheritance and Composition/program.py
+++ Inheritance and Composition/program.py
@@ -1,11 +1,20 @@
 from hr import PayrollSystem
 from productivity import ProductivitySystem
 from employees import EmployeeDatabase
+import json
 
-productivity_system = ProductivitySystem()
+""" productivity_system = ProductivitySystem()
 payroll_system = PayrollSystem()
 employee_database = EmployeeDatabase()
 
 employees = employee_database.employees()
 productivity_system.track(employees, 40)
-payroll_system.calculate_payroll(employees)
+payroll_system.calculate_payroll(employees) """
+
+def print_json(d):
+    print(json.dumps(d, indent=2))
+
+employees = EmployeeDatabase().employees()
+
+for employee in employees:
+    print_json(employee.to_dict())
