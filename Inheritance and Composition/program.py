d99371fbc5cb064fe473a059d74fb51c17c81fb6
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-04-24 14:43:44 -0300

    adjust main program to test new feature

--- Inheritance and Composition/program.py
+++ Inheritance and Composition/program.py
@@ -1,15 +1,17 @@
 import json
 
-from hr import calculate_payroll
+from hr import LTDPolicy, calculate_payroll
 from productivity import track
 from employees import Employee, employees
 
 def print_dict(d):
     print(json.dumps(d, indent=2))
 
-calculate_payroll(employees())
-track(employees(), 40)
+employees_data = employees()
 
-temp_secretary = Employee(5)
-print('Temporary Secretary')
-print_dict(temp_secretary.to_dict())
+sales_employee = employees_data[2]
+ltd_policy = LTDPolicy()
+sales_employee.apply_payroll_policy(ltd_policy)
+
+track(employees_data, 40)
+calculate_payroll(employees_data)
